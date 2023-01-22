f3dd4bad5c1219f069fbb7a12f34ef339ed0c4fe
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2023-01-22 16:53:08 +0100

    fixed scroll animation

--- scripts/filter-bar-scroll.js
+++ scripts/filter-bar-scroll.js
@@ -1,22 +1,27 @@
 const body = document.body;
 let lastScroll = 0; 
 
-window.addEventListener('scroll', () => {
-    let currentScroll = window.pageYOffset;
+ 
+    window.addEventListener('scroll', () => {
 
-    if (currentScroll < 0){
-        body.classList.remove('scroll-up');
-    }
-
-    if (currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
-        body.classList.remove('scroll-up')
-        body.classList.add('scroll-down')
-    }
-
-    if (currentScroll < lastScroll && body.classList.contains('scroll-down')) {
-        body.classList.remove('scroll-down')
-        body.classList.add('scroll-up')
-    }
+        if(window.screen.width > 769) {
+            let currentScroll = window.pageYOffset;
     
-    lastScroll = currentScroll;
-})
+        if (currentScroll < 0){
+            body.classList.remove('scroll-up');
+        }
+    
+        if (currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
+            body.classList.remove('scroll-up')
+            body.classList.add('scroll-down')
+        }
+    
+        if (currentScroll < lastScroll && body.classList.contains('scroll-down')) {
+            body.classList.remove('scroll-down')
+            body.classList.add('scroll-up')
+        }
+        
+        lastScroll = currentScroll;
+        }
+        
+    })
